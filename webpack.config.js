4921ce4cf8d2a09af67da16c1ffbbc6912766d96
Arno Fiva <arnofiva@gmail.com>
2022-11-02 16:51:27 +0100

    Provide Webpack setup that creates bundle in /dist for dev and prod profile

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,13 @@
 const path = require('path');
 
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 module.exports = {
   mode: 'development',
-  entry: './src/app.ts',
+  entry: {
+    index: ['./public/css/styles.css', './src/app.ts']
+  },
   module: {
     rules: [
       {
@@ -10,14 +15,40 @@ module.exports = {
         use: 'ts-loader',
         exclude: /node_modules/,
       },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader'
+        ]
+      },
     ],
   },
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
   },
   output: {
-    path: path.resolve(__dirname, './public'),
-    publicPath: '/js/',
-    filename: 'app.bundle.js',
+    path: path.join(__dirname, 'dist'),
+    chunkFilename: 'chunks/[id].js',
+    publicPath: '',
+    clean: true
   },
+  devServer: {
+    static: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 3001,
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      title: 'ArcGIS API  for JavaScript',
+      template: './public/index.html',
+      filename: './index.html',
+      chunksSortMode: 'none',
+      inlineSource: '.(css)$'
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].[chunkhash].css",
+      chunkFilename: "[id].css"
+    })
+  ]
 };
